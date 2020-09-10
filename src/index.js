8523890692fdc7cf8e41b5f672ef3011d17e3aa9
Victor Ramirez <victor.ramirez02@estudiant.upf.edu>
2020-09-10 13:48:30 +0200

    dispatch/subscribe emitter working

--- src/index.js
+++ src/index.js
@@ -1,7 +1,47 @@
-import React from "react";
+import React, {useState} from "react";
+import Emitter from "./emitter";
 
 function Index() {
-  return <h1>test string</h1>;
+  const [deck, setDeck] = useState([]);
+  const addNotification = (event) => {
+    setDeck([
+      ...deck, 
+      {
+        timestamp: +new Date(),
+        type: event.type || 'info',
+        text: event.text || 'Notification'
+      }
+    ])
+  }
+
+  Emitter.subscribe('add', (event) => addNotification(event));
+
+  return (
+    <ul>
+      {deck.map((item, i) => (
+        <li key={item.timestamp} style={{backgroundColor: "#00f2"}}>{item.text}</li>
+      ))}
+    </ul>
+  );
+}
+
+export function TestDispatcher() {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    Emitter.dispatch('add', {type: 'error', text: inputValue})
+  }
+
+  return (
+    <form onSubmit={e => handleSubmit(e)}>
+      <h2>Test dispatcher</h2>
+      <fieldset>
+        <input type="text" value={inputValue} onChange={e => setInputValue(e.target.value)}/>
+        <button type="submit">send</button>
+      </fieldset>
+    </form>
+  );
 }
 
 export default Index;
